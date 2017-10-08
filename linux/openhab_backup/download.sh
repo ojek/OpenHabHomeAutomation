1921c2049d4b661d6d2eb567bb4e3deae5b24bbe
ojek <galecki.maciek@gmail.com>
2017-10-08 16:14:01 +0200

    added OH restart to the download script

--- linux/openhab_backup/download.sh
+++ linux/openhab_backup/download.sh
@@ -5,3 +5,4 @@ cd ~/git/OpenHabHomeAutomation
 git pull https://github.com/ojek/OpenHabHomeAutomation
 rm -rf /etc/openhab2/*
 cp -r ~/git/OpenHabHomeAutomation/openhab/etc/openhab2/* /etc/openhab2/
+sudo systemctl restart openhab2.service
