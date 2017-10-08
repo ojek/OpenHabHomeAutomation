3f43960d846312cb5eeee81972894ece90017721
ojek <galecki.maciek@gmail.com>
2017-10-08 15:42:37 +0200

    upload script commit message changed

--- linux/openhab_backup/upload.sh
+++ linux/openhab_backup/upload.sh
@@ -5,5 +5,5 @@ cd ~/git/OpenHabHomeAutomation
 rm -rf ~/git/OpenHabHomeAutomation/openhab/etc/openhab2/*
 cp -r /etc/openhab2/* ~/git/OpenHabHomeAutomation/openhab/etc/openhab2/
 git add --all
-git commit -am 'added openhab etc folder structure'
+git commit -am 'Autobackup commit'
 git push
