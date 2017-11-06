b8db015caf0317d7cb60e5eafcf2644494c2decc
ojek <galecki.maciek@gmail.com>
2017-11-06 23:49:07 +0100

    system widget adjustments

--- linux/openhab_backup/download.sh
+++ linux/openhab_backup/download.sh
@@ -5,4 +5,5 @@ cd /home/pi/git/OpenHabHomeAutomation
 git pull https://github.com/ojek/OpenHabHomeAutomation
 rm -rf /etc/openhab2/*
 cp -r /home/pi/git/OpenHabHomeAutomation/openhab/etc/openhab2/* /etc/openhab2/
+./home/pi/git/OpenHabHomeAutomation/linux/openhab_backup/postDownloadScripts.sh
 sudo systemctl restart openhab2.service
---
+++ linux/openhab_backup/postDownloadScripts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# OpenHab post git update scripts
+
+sudo chmod a+x /etc/openhab2/scripts/rpiTemp.sh
