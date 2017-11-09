b5f4c00f255104acc3ebe43a70c63d393eef0c2c
ojek <galecki.maciek@gmail.com>
2017-11-09 23:39:27 +0100

    custom css

--- linux/openhab_backup/download.sh
+++ linux/openhab_backup/download.sh
@@ -10,4 +10,6 @@ cp -r /home/pi/git/OpenHabHomeAutomation/openhab/etc/openhab2/* /etc/openhab2/
 chmod a+x /etc/openhab2/scripts/rpiTemp.sh
 #post download scripts end
 
-sudo systemctl restart openhab2.service
+if [ "$1" != 0 ] ; then
+    sudo systemctl restart openhab2.service
+fi
