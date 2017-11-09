4c0fa0154a96408ab8c51fddc9053b024107aa4a
ojek <galecki.maciek@gmail.com>
2017-11-09 23:56:50 +0100

    fixed download.sh

--- linux/openhab_backup/download.sh
+++ linux/openhab_backup/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # OpenHab git update
 
-cd /home/pi/git/OpenHabHomeAutomation 
+cd /home/pi/git/OpenHabHomeAutomation
 git pull https://github.com/ojek/OpenHabHomeAutomation
 rm -rf /etc/openhab2/*
 cp -r /home/pi/git/OpenHabHomeAutomation/openhab/etc/openhab2/* /etc/openhab2/
