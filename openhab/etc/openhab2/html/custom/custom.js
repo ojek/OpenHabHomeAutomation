92f1c0f27ce4bec701091a2c2cdd75ffb45fb59a
ojek <galecki.maciek@gmail.com>
2017-11-12 00:52:17 +0100

    customjs

--- openhab/etc/openhab2/html/custom/custom.js
+++ openhab/etc/openhab2/html/custom/custom.js
@@ -1,7 +1,15 @@
 (function(){
     var newscript = document.createElement('script');
-       newscript.type = 'text/javascript';
-       newscript.async = true;
-       newscript.src = '/static/custom/jQuery/jquery-3.2.1.min.js';
+    newscript.type = 'text/javascript';
+    newscript.async = true;
+    newscript.src = '/static/custom/jQuery/jquery-3.2.1.min.js';
     (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
-  })();
+})();
+
+function loadIframes(){
+    var iframes = $('div.iframe');
+    for(var i = 0; i < iframes.length; i++){
+        var text = iframes[i].text();
+        iframes.appendChild('<iframe src="'+text+'"></iframe>');
+    }
+}
