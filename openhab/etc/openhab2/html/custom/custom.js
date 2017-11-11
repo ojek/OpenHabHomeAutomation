9f144d214ea8d05754a7eaeaf889844aaacbdc59
ojek <galecki.maciek@gmail.com>
2017-11-12 00:38:48 +0100

    customjs

--- openhab/etc/openhab2/html/custom/custom.js
+++ openhab/etc/openhab2/html/custom/custom.js
@@ -1,4 +1,4 @@
-angular.module('app', [])
-.config(function($sceProvider) {
+var app = angular.module('app', ['ngSanitize']);
+app.config(['$sceProvider',function($sceProvider){
     $sceProvider.enabled(false);
-});
+}]);
