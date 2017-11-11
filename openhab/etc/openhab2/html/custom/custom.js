b090666dd455d6d10a51b30c6e0a60adfdb613c7
ojek <galecki.maciek@gmail.com>
2017-11-12 00:47:09 +0100

    added jquery

--- openhab/etc/openhab2/html/custom/custom.js
+++ openhab/etc/openhab2/html/custom/custom.js
@@ -1,4 +1,7 @@
-var app = angular.module('app', ['ngSanitize']);
-app.config(['$sceProvider',function($sceProvider){
-    $sceProvider.enabled(false);
-}]);
+(function(){
+    var newscript = document.createElement('script');
+       newscript.type = 'text/javascript';
+       newscript.async = true;
+       newscript.src = '/static/custom/jQuery/jquery-3.2.1.min.js';
+    (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+  })();
