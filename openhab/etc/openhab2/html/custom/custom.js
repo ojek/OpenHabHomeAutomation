7f098a3ca283d14ba5eb8e5df629034451a80ba6
ojek <galecki.maciek@gmail.com>
2017-11-12 01:02:11 +0100

    customjs

--- openhab/etc/openhab2/html/custom/custom.js
+++ openhab/etc/openhab2/html/custom/custom.js
@@ -1,15 +1,23 @@
-(function(){
-    var newscript = document.createElement('script');
-    newscript.type = 'text/javascript';
-    newscript.async = true;
-    newscript.src = '/static/custom/jQuery/jquery-3.2.1.min.js';
-    (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
-})();
+if (typeof loadJQuery != "function") { 
+    function loadJQuery(){
+        var newscript = document.createElement('script');
+        newscript.type = 'text/javascript';
+        newscript.async = true;
+        newscript.src = '/static/custom/jQuery/jquery-3.2.1.min.js';
+        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+    }
+    loadJQuery();
+}
 
-function loadIframes(){
-    var iframes = $('div.iframe');
-    for(var i = 0; i < iframes.length; i++){
-        var text = iframes[i].text();
-        iframes.appendChild('<iframe src="'+text+'"></iframe>');
+if (typeof loadIframes != "function") { 
+    function loadIframes(){
+        var iframeDivs = $('div.iframe');
+        for(var i = 0; i < iframeDivs.length; i++){
+            var text = $(iframeDivs[i]).text();
+            $(iframeDivs[i]).text('');
+            $(iframeDivs[i]).append('<iframe src="'+text+'"></iframe>');
+        }
     }
 }
+
+loadIframes();
