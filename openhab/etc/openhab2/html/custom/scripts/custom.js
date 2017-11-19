36561105aeaaf00ce7cdaf94ac4a0f60e37c60fc
ojek <galecki.maciek@gmail.com>
2017-11-19 22:46:45 +0100

    auto reload

--- openhab/etc/openhab2/html/custom/scripts/custom.js
+++ openhab/etc/openhab2/html/custom/scripts/custom.js
@@ -1,6 +1,12 @@
 if (interval === 'undefined'){
     var interval = null;
 }
+if (repeatTime === 'undefined'){
+    var repeatTime = 300000; //5m
+}
+if (repeatInterval === 'undefined'){
+    var repeatInterval = null;
+}
 
 if (typeof loadJQuery != "function") { 
     function loadJQuery(){
@@ -16,26 +22,26 @@ if (typeof loadJQuery != "function") {
 if (typeof loadContent != "function") {   
     interval = window.setInterval(function(){
         if (typeof jQuery != 'undefined' && typeof loadContent != "function") {
-            function loadContent(selector, element){
-                var iframeDivs = $(selector);
-                for(var i = 0; i < iframeDivs.length; i++){
-                    var text = $(iframeDivs[i]).text();
+            function loadContent(source, element){
+                var srcElems = $(source);
+                for(var i = 0; i < srcElems.length; i++){
+                    var text = $(srcElems[i]).text();
                     if (text.indexOf('#loading_') === -1) continue;
                     text = text.replace('#loading_', '');
-                    $(iframeDivs[i]).text('');
-                    $(iframeDivs[i]).append(element.replace('@replace@', text));
+                    var adjacentElem = $(srcElems[i]).next();
+                    $(adjacentElem).text('');
+                    $(adjacentElem).append(element.replace('@replace@', text));
                 }
             };
             clearInterval(interval);
-            loadContent('div.iframe', '<iframe src="@replace@"></iframe>');
-            loadContent('div.icon', '<svg viewBox="0 0 48 48"><use xlink:href="/static/custom/icons/icons.svg#@replace@"/></svg>');
-        } else {
+            loadContent('div.iframe src', '<iframe src="@replace@"></iframe>');
+
+            if (repeatInterval === null) {
+                repeatInterval = window.setInterval(function(){loadContent('div.iframe src', '<iframe src="@replace@"></iframe>')},repeatTime);                
+            }
+        } 
+        else {
             clearInterval(interval);
         }
     }, 1000);
-}
-
-if (typeof loadContent === "function") { 
-    loadContent('div.iframe', '<iframe src="@replace@"></iframe>');
-    loadContent('div.icon', '<svg viewBox="0 0 48 48"><use xlink:href="/static/custom/icons/icons.svg#@replace@"/></svg>');
 }
