2b0fd21a9f5d01b1ff36452982c76981d945bc68
ojek <galecki.maciek@gmail.com>
2018-04-22 20:45:41 +0200

    Added miio binding

--- arduino/Sonoff_T1_LivingRoom_Main/user_config.h
+++ arduino/Sonoff_T1_LivingRoom_Main/user_config.h
@@ -39,7 +39,7 @@
 //#define MY_LANGUAGE            zh-CN           // Chinese (Simplified) in China
 
 // -- Project -------------------------------------
-#define PROJECT                "Sonoff_S20_Bedroom_Main"          // PROJECT is used as the default topic delimiter and OTA file name
+#define PROJECT                "Sonoff_T1_LivingRoom_Main"          // PROJECT is used as the default topic delimiter and OTA file name
                                                  //   As an IDE restriction it needs to be the same as the main .ino file
 
 #define CFG_HOLDER             1                 // [Reset 1] Change this value to load following default configuration parameters
