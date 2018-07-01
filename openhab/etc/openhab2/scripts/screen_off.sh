99f50b9c4f152e55cda123dcd589a7566f7ec5e5
ojek <galecki.maciek@gmail.com>
2018-07-01 23:59:10 +0200

    screen settings

--- openhab/etc/openhab2/scripts/screen_off.sh
+++ openhab/etc/openhab2/scripts/screen_off.sh
@@ -1,1 +1,1 @@
-sudo tvservice -o
+vcgencmd display_power 0
--- openhab/etc/openhab2/scripts/screen_on.sh
+++ openhab/etc/openhab2/scripts/screen_on.sh
@@ -1,2 +1,1 @@
-sudo tvservice -p && sudo chvt 2 && sudo chvt 7
-DISPLAY=:0 chromium-browser -kiosk http://192.168.0.5:8080/habpanel/index.html#/view/SmartMirror?kiosk=on
+vcgencmd display_power 1
