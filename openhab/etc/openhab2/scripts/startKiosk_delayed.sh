2db82fde9cb76e76a857a1049d72b73e1a70ddd1
ojek <galecki.maciek@gmail.com>
2018-08-16 19:45:31 +0200

    removed unused script

--- openhab/etc/openhab2/scripts/startKiosk_delayed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sleep 60
-unclutter &
-matchbox-window-manager &
-iceweasel http://192.168.0.5:8080/habpanel/index.html#/view/SmartMirror?kiosk=on --display=:0 &
-sleep 15s;
-xte "key F11" -x:0
